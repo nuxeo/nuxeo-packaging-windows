7c0126a0980573bc068de19abfefb61441dccb7b
Mathieu Guillaume <mg@nuxeo.com>
2019-01-15 16:20:46 +0100

    NXBT-2399: (try to) switch default branch to master

--- windows-installer.groovy
+++ windows-installer.groovy
@@ -22,7 +22,8 @@ properties([[$class: 'BuildDiscarderProperty',
             strategy: [$class: 'LogRotator', artifactDaysToKeepStr: '', artifactNumToKeepStr: '1', daysToKeepStr: '60', numToKeepStr: '60']],
             [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false],
             [$class: 'ParametersDefinitionProperty', parameterDefinitions: [
-            [$class: 'StringParameterDefinition', defaultValue: '6.0-SNAPSHOT', description: 'Product version to build', name: 'NUXEO_VERSION'],
+            [$class: 'StringParameterDefinition', defaultValue: '10.10-SNAPSHOT', description: 'Product version to build', name: 'NUXEO_VERSION'],
+            [$class: 'StringParameterDefinition', defaultValue: 'master', description: 'Branch to build', name: 'BRANCH'],
             [$class: 'StringParameterDefinition', defaultValue: '', description: 'Optional - Alternate URL to download the distribution from instead of the default Maven artifact download. For instance: http://community.nuxeo.com/static/snapshots/nuxeo-server-tomcat-10.3-SNAPSHOT.zip', name: 'DISTRIBUTION_URL'],
             [$class: 'BooleanParameterDefinition', defaultValue: true, description: 'Publish .exe package', name: 'PUBLISH_EXE'],
             [$class: 'StringParameterDefinition', defaultValue: '/var/www/community.nuxeo.com/static/staging/', description: 'Staging publishing destination path (for scp)', name: 'STAGING_PATH'],
@@ -32,7 +33,7 @@ properties([[$class: 'BuildDiscarderProperty',
 node('SLAVE') {
   timestamps {
     timeout(time: 240, unit: 'MINUTES') {
-      checkout([$class: 'GitSCM', branches: [[name: '*/feature-NXBT-2399-split&fix']], browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/nuxeo/nuxeo-packaging-windows'], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PathRestriction', excludedRegions: '3rdparties/.*', includedRegions: '']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/nuxeo/nuxeo-packaging-windows']]])
+      checkout([$class: 'GitSCM', branches: [[name: $BRANCH]], browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/nuxeo/nuxeo-packaging-windows'], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PathRestriction', excludedRegions: '3rdparties/.*', includedRegions: '']], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/nuxeo/nuxeo-packaging-windows']]])
       sh '''#!/bin/bash -ex
         MAVEN_OPTS="-Xmx512m -Xmx2048m"
         if [ -n "$DISTRIBUTION_URL" ]; then
